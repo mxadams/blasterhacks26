e2b68339edc20a3e8909509bbe4cf69e5004908a
mxadams <xavyabides@gmail.com>
2026-04-04 00:25:57 -0600

    add cli options

--- terminal_panel_container.gd
+++ terminal_panel_container.gd
@@ -10,9 +10,16 @@ func _on_command_entered(text: String) -> void:
 	var command = text.strip_edges().to_lower().split(" ", false)
 	if command.size() == 0:
 		return
+	rich_text_label.text += "\n\n--->  " + text
 	match command[0]:
+		"help":
+			rich_text_label.text += """
+help - Get help with the CLI
+status - Print territory percentages for each faction and number of remaining turns
+end - End turn
+exit - Exit the game"""
 		"exit":
 			get_tree().quit()
 		_:
-			rich_text_label.text += "\n\nUnknown Command: " + text
+			rich_text_label.text += "\nUnknown Command: " + text
 	line_edit.clear()
